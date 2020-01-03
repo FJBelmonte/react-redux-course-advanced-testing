74f5764229ca16003c072231beebd035a52e79e5
Fernando Belmonte <fernando.belmonte@live.com>
2020-01-03 20:20:15 -0300

    lesson 22

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,19 +1,8 @@
 import App from "../App";
 import React from "react";
-import ReactDOM from "react-dom";
-
+import { shallow } from "enzyme";
+import CommentBox from "../../components/CommentBox";
 it("shows a comment box", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it("shows a comment box", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  // Look inside the div
-  // And checks to see if the CommentBox is in there
-  expect(div.innerHTML).toContain("Comment Box");
-
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
