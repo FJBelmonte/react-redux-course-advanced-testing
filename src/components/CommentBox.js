28e59200cda016e393f09e46d10c31840fdf82ca
Fernando Belmonte <fernando.belmonte@live.com>
2020-01-03 21:01:20 -0300

    lesson 32

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default () => {
-  return <div>Comment Box</div>;
-};
+export default function CommentBox() {
+  const [comment, setComment] = useState("");
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        setComment("");
+      }}>
+      <h4>Add a comment</h4>
+      <textarea value={comment} onChange={e => setComment(e.target.value)} />
+      <div>
+        <button>Submit comment</button>
+      </div>
+    </form>
+  );
+}
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,8 +1,19 @@
-import App from "../App";
+import App from "components/App";
 import React from "react";
 import { shallow } from "enzyme";
-import CommentBox from "../../components/CommentBox";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
 it("shows a comment box", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it("shows a comment list", () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
