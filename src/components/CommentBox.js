ed8c57b687f7b8c88716dc3659b760db39d99654
Fernando Belmonte <fernando.belmonte@live.com>
2020-01-23 23:17:09 -0300

    lesson 45

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import * as actions from "actions";
 
 export default function CommentBox() {
+  const dispatch = useDispatch();
+
   const [comment, setComment] = useState("");
   return (
     <form
       onSubmit={e => {
         e.preventDefault();
+        dispatch(actions.saveComment(e));
         setComment("");
       }}>
       <h4>Add a comment</h4>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import reducers from "reducers";
 import { Provider } from "react-redux";
-import createStore from "redux";
 
 ReactDOM.render(
   <Provider store={createStore(reducers, {})}>
