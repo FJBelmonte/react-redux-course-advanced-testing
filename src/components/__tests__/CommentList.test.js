40dd82b3a71af6e7a9397094be92bf7dc0654c73
Fernando Belmonte <fernando.belmonte@live.com>
2020-02-05 21:21:51 -0300

    lesson 52

---
+++ src/components/__tests__/CommentList.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { mount } from "enzyme";
+import CommentList from "components/CommentList";
+import Root from "Root";
+
+let wrapped;
+
+beforeEach(() => {
+  const INITIAL_STATE = { comments: ["Comment1", "Comment2"] };
+  wrapped = mount(
+    <Root initialState={INITIAL_STATE}>
+      <CommentList />
+    </Root>
+  );
+});
+
+it("creates one <li/> per comment", () => {});
