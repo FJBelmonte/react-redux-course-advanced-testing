79020ab0f015bc97e162aeeb9ecc371a6ff45a66
Fernando Belmonte <fernando.belmonte@live.com>
2020-02-22 18:49:15 -0300

    lesson 55

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,14 @@
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import reduxPromise from "redux-promise";
 import reducers from "reducers";
 import React from "react";
 
 export default function Root({ children, initialState = {} }) {
-  return (
-    <Provider store={createStore(reducers, initialState)}>{children}</Provider>
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(reduxPromise)
   );
+  return <Provider store={store}>{children}</Provider>;
 }
