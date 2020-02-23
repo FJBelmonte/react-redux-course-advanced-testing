d868c1fec5f7dd9cdabc519727ed773bb62193b3
Fernando Belmonte <fernando.belmonte@live.com>
2020-02-23 13:53:57 -0300

    lesson 59

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -3,4 +3,10 @@ import { mount } from "enzyme";
 import Root from "Root";
 import App from "components/App";
 
-it("can fetch a list of coments and display them", () => {});
+it("can fetch a list of coments and display them", () => {
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
+});
