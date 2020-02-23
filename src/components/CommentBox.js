206c55791104ecb15dfbf7cb685602361465737a
Fernando Belmonte <fernando.belmonte@live.com>
2020-02-23 13:49:51 -0300

    lesson 58

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -7,17 +7,23 @@ export default function CommentBox() {
 
   const [comment, setComment] = useState("");
   return (
-    <form
-      onSubmit={e => {
-        e.preventDefault();
-        dispatch(actions.saveComment(comment));
-        setComment("");
-      }}>
-      <h4>Add a comment</h4>
-      <textarea value={comment} onChange={e => setComment(e.target.value)} />
-      <div>
-        <button>Submit comment</button>
-      </div>
-    </form>
+    <div>
+      {" "}
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          dispatch(actions.saveComment(comment));
+          setComment("");
+        }}>
+        <h4>Add a comment</h4>
+        <textarea value={comment} onChange={e => setComment(e.target.value)} />
+        <div>
+          <button>Submit comment</button>
+        </div>
+      </form>
+      <button onClick={dispatch(actions.fetchComments())}>
+        Fetch Comments
+      </button>
+    </div>
   );
 }
