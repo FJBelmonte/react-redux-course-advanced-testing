338fed39e311cad409d30220cf6c3bab61c6ad57
Fernando Belmonte <fernando.belmonte@live.com>
2020-04-19 16:11:41 -0300

    lesson 78 + hooks

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,44 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import * as actions from "actions";
+import requireAuth from "../components/requireAuth";
 
 export default function CommentBox() {
-  const state = useSelector((state) => state);
-  const dispatch = useDispatch();
-  const history = useHistory();
   const [comment, setComment] = useState("");
-
-  useEffect(() => {
-    if (!state.auth) {
-      history.push("/");
-    }
-  }, [history, state.auth]);
-
+  const dispatch = useDispatch();
   return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          dispatch(actions.saveComment(comment));
-          setComment("");
-        }}
-      >
-        <h4>Add a comment</h4>
-        <textarea
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        />
-        <div>
-          <button>Submit comment</button>
-        </div>
-      </form>
-      <button
-        className='fetch-comments'
-        onClick={() => dispatch(actions.fetchComments())}
-      >
-        Fetch Comments
-      </button>
-    </div>
+    <requireAuth>
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            dispatch(actions.saveComment(comment));
+            setComment("");
+          }}
+        >
+          <h4>Add a comment</h4>
+          <textarea
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <div>
+            <button>Submit comment</button>
+          </div>
+        </form>
+        <button
+          className='fetch-comments'
+          onClick={() => dispatch(actions.fetchComments())}
+        >
+          Fetch Comments
+        </button>
+      </div>
+    </requireAuth>
   );
 }
