9e16fc2216ad3e8ab19f43cf39b524cf36a1b30d
Fernando Belmonte <fernando.belmonte@live.com>
2020-04-19 14:34:14 -0300

    lesson 73

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,8 @@
 import commentsReducer from "reducers/comments.reducer";
+import authReducer from "reducers/auth.reducer";
 import { combineReducers } from "redux";
 
-export default combineReducers({ comments: commentsReducer });
+export default combineReducers({
+  comments: commentsReducer,
+  auth: authReducer,
+});
