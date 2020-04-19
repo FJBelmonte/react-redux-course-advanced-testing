78ab9c6fc66311738ccc0ea062927582c1e986d2
Fernando Belmonte <fernando.belmonte@live.com>
2020-04-19 15:01:38 -0300

    lesson 75 + hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { Route, Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import * as actions from "../actions";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
 export default function App() {
   const state = useSelector((state) => state);
+  const dispatch = useDispatch();
   function renderButton() {
-    if (state.auth) {
-      return <button>Sign out</button>;
-    } else {
-      return <button>Sign in</button>;
-    }
+    return (
+      <button
+        onClick={() => {
+          dispatch(actions.changeAuth(!state.auth));
+        }}
+      >
+        {state.auth ? "Sign out" : "Sign in"}
+      </button>
+    );
   }
   function renderHeader() {
     return (
