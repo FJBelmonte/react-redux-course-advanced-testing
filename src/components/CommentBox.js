8c047c6fd7a1f98c9e8cdb1b13feb11f263ca306
Fernando Belmonte <fernando.belmonte@live.com>
2020-04-19 15:30:01 -0300

    lesson 77

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,29 +1,42 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
 import * as actions from "actions";
 
 export default function CommentBox() {
+  const state = useSelector((state) => state);
   const dispatch = useDispatch();
-
+  const history = useHistory();
   const [comment, setComment] = useState("");
+
+  useEffect(() => {
+    if (!state.auth) {
+      history.push("/");
+    }
+  }, [history, state.auth]);
+
   return (
     <div>
-      {" "}
       <form
-        onSubmit={e => {
+        onSubmit={(e) => {
           e.preventDefault();
           dispatch(actions.saveComment(comment));
           setComment("");
-        }}>
+        }}
+      >
         <h4>Add a comment</h4>
-        <textarea value={comment} onChange={e => setComment(e.target.value)} />
+        <textarea
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
         <div>
           <button>Submit comment</button>
         </div>
       </form>
       <button
         className='fetch-comments'
-        onClick={() => dispatch(actions.fetchComments())}>
+        onClick={() => dispatch(actions.fetchComments())}
+      >
         Fetch Comments
       </button>
     </div>
